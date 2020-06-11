b4d772980669eba0a67541070e49a3101cdd0596
sid-38 <sidharthanil98@gmail.com>
2020-06-11 09:54:22 +0530

    Preprocess hanging bracket fixed

--- preprocessing/test.js
+++ preprocessing/test.js
@@ -1,59 +1,75 @@
+
 function split(inp_text){
 
-	// WORD SPLITTER
-
-	inp_text = inp_text.concat(" ");
+	inp_text = inp_text.concat("^");
 	words = [];
 	word = "";
 	paranthesis = 0;
-	
-	for (letter of inp_text){
 
-		if((letter == " " || letter=="\n")&& paranthesis == 0){
+	for(letter of inp_text){
+		if(letter == " " && paranthesis == 0){
 			if(word != "")
 				words.push(word);
 			word = "";
 			continue;
 		}
 
-		if(letter =="("){
-			if(paranthesis == 0){
-				if(word != ""){
-					words.push(word.concat("("));
-					word = "";
-				}
-				else{
+		if(letter ==  "("){
+			paranthesis++;
+			if (paranthesis == 1)
+			{
+				if(word == ""){
 					words[words.length - 1] = words[words.length -1].concat("(");
 				}
-				paranthesis++;
+				else{
+					word = word.concat("(");
+					words.push(word);
+					word = "";
+				}
 				continue;
 			}
-			paranthesis++;
+			
 		}
 
-		if(letter ==")"){
+		if(letter == ")"){
 			paranthesis--;
-			if(paranthesis == 0){
-				if (word!= ""){
+			if (paranthesis==0)
+			{
+				if(word != ""){
 					words.push(word);
-					word="";
-					words.push(")");
-					continue;
+					word = "";
+					
 				}
 				words.push(")");
+				continue;
 			}
 		}
 
+		if(letter == "^"){
+			words.push(word);
+			word="";
+			continue;
+		}
 		word = word.concat(letter);
 	}
+	
 	return words;
+	
+}
+
+
+function is_keyword(word, keyword_array){
+	for(keyword of keyword_array){
+		
+	}
 }
 
 
 function preprocess(inp_text, keywords_array){
 	var words = split(inp_text);
+	// console.log(words);
 	for(var i =0; i< words.length; i++){
-		if(!(keywords_array.includes(words[i]))){
+		if(!(keywords_array.includes(words[i].toUpperCase()))){
 			words[i] = "^";
 		}
 	}
@@ -63,8 +79,8 @@ function preprocess(inp_text, keywords_array){
 }
 
 
-keywords = ["INSERT","INTO","VALUES(",")"];
-text = "INSERT INTO TABLE1 VALUES(BLAH, Yay, Test)"
+keywords = ["INSERT","INTO","VALUES(","VALUES",")"];
+text = "insert into table1 values(my friend) test"
 
 console.log(preprocess(text,keywords));
 
